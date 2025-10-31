3002f6ef4d11239835f3e541497e6d3c71996ce1
Devi prasad <prasad20697@gmail.com>
2025-10-31 22:58:12 +0530

    form component

--- src/components/multi-step-form.tsx
+++ src/components/multi-step-form.tsx
@@ -1,17 +1,21 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import {useForm} from "react-hook-form"
-import { personalInfoSchema } from "../types"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { personalInfoSchema, type StepFormData } from "../types";
 
 const MultistepForm = () => {
-   // custom hook
+	// custom hook
 
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		trigger,
+		setValue,
+		reset,
+	} = useForm<StepFormData>({
+		// resolver: zodResolver()
+	});
+	return <div>Multi step Form</div>;
+};
 
-   useForm({
-      // resolver: zodResolver()
-   })
-  return (
-    <div>Multi step Form</div>
-  )
-}
-
-export default MultistepForm
+export default MultistepForm;

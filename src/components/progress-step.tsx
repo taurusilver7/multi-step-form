2b962426a57e776bd0dff732918eb834550b52e5
Devi prasad <prasad20697@gmail.com>
2025-11-01 16:55:41 +0530

    form header progress

---
+++ src/components/progress-step.tsx
@@ -0,0 +1,14 @@
+
+import type { Step } from "../types";
+
+const ProgressSteps = ({
+	currentStep,
+	steps,
+}: {
+	currentStep: number;
+	steps: Step[];
+}) => {
+	return <div>Progress Steps</div>;
+};
+
+export default ProgressSteps;

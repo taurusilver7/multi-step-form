7405485dd11e19617ee7c89bdd6db631a018591f
Devi prasad <prasad20697@gmail.com>
2025-11-01 16:55:31 +0530

    multi step form

--- src/components/multi-step-form.tsx
+++ src/components/multi-step-form.tsx
@@ -1,9 +1,34 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { personalInfoSchema, type StepFormData } from "../types";
+import { useMultiStepForm } from "../hooks/use-multi-step-form";
+import { useEffect } from "react";
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
+import ProgressSteps from "./progress-step";
+import {
+	BillingInfoStep,
+	PersonalInfoStep,
+	ProfessionalInfoStep,
+} from "./steps";
+import { Button } from "./ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const MultistepForm = () => {
 	// custom hook
+	const {
+		getCurrentStepSchema,
+		gotoNextStep,
+		gotoPrevStep,
+		handleFinalSubmit,
+		updateFormData,
+		resetForm,
+		currentStep,
+		formData,
+		isFirstStep,
+		isLastStep,
+		isSubmitted,
+		steps,
+	} = useMultiStepForm();
 
 	const {
 		register,
@@ -13,9 +38,50 @@ const MultistepForm = () => {
 		setValue,
 		reset,
 	} = useForm<StepFormData>({
-		// resolver: zodResolver()
+		resolver: zodResolver(getCurrentStepSchema()),
+		mode: "onChange",
+		defaultValues: formData,
 	});
-	return <div>Multi step Form</div>;
+
+	useEffect(() => {
+		reset(formData);
+	}, [currentStep, formData, reset]);
+
+	const onNext = async (data: StepFormData) => {
+		// Check for input validations manually
+		// Merge current step data with prev data.
+	};
+
+	return (
+		<div className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
+			<Card className="w-full max-w-2xl">
+				<CardHeader>
+					<ProgressSteps currentStep={currentStep} steps={steps} />
+				</CardHeader>
+				<CardContent className="space-y-6">
+					{currentStep === 0 && <PersonalInfoStep />}
+					{currentStep === 1 && <ProfessionalInfoStep />}
+					{currentStep === 2 && <BillingInfoStep />}
+
+					<div className="flex items-center justify-between pt-4">
+						<Button
+							onClick={gotoPrevStep}
+							disabled={isFirstStep}
+							type="button"
+							variant={"outline"}
+						>
+							<ChevronLeft className="size-4 mr-1" />
+							Previous
+						</Button>
+						<Button type="button" onClick={handleSubmit(onNext)}>
+							{isLastStep ? "Submit" : "Next"}
+							{!isLastStep && <ChevronRight className="size-4 mr-1" />}
+						</Button>
+					</div>
+				</CardContent>
+			</Card>
+		</div>
+	);
 };
 
 export default MultistepForm;

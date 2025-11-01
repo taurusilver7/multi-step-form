9327d754a6b8bb7c533515081964f48d44024ebf
Devi prasad <prasad20697@gmail.com>
2025-11-01 16:55:52 +0530

    induvidual form steps

---
+++ src/components/steps.tsx
@@ -0,0 +1,11 @@
+const PersonalInfoStep = () => {
+	return <div>Personal Info Step</div>;
+};
+const ProfessionalInfoStep = () => {
+	return <div>Professional Info Step</div>;
+};
+const BillingInfoStep = () => {
+	return <div>Billing Info Step</div>;
+};
+
+export { PersonalInfoStep, ProfessionalInfoStep, BillingInfoStep };
